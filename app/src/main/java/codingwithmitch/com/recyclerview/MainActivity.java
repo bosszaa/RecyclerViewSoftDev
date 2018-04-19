c96e0a52aa5fd27710884a23e26949d44b5504e9
bosszaa <bosschomphu@gmail.com>
2018-04-19 22:07:27 +0700

    2

--- app/src/main/java/codingwithmitch/com/recyclerview/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/recyclerview/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private  String [] mDrawerTitle = {"History", "Colours and badge", "Stadium", "Support", "Ownership and finances", "Liverpool in popular culture", "Players", "Club officials", "Honours", "References"};
+    private  String [] mDrawerTitle = {"History", "Song", "Cartoon", "Sport", "Fashion", "LifeStyle", "Players", "Pop", "Rock", "Indy"};
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private ListView mListView;
@@ -112,42 +112,30 @@ public class MainActivity extends AppCompatActivity {
     private void initImageBitmaps(){
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
-        mImageUrls.add("http://givemesport.azureedge.net/images/18/02/11/cffd64648da49950386a10dcc6bdf511/960.jpg");
-        mNames.add("Loris Karius");
+        mImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/a/a7/Mahafather.jpg");
+        mNames.add("Mahafather");
 
-        mImageUrls.add("http://e0.365dm.com/17/12/16-9/20/skysports-premier-league-football-trent-alexander-arnold-trent-alexander-arnold_4192491.jpg?20171226190902");
-        mNames.add("Trent Alexander-Arnold");
+        mImageUrls.add("https://www.dochord.com/wp-content/uploads/2017/11/D-Gerrard.jpg");
+        mNames.add("D-Gerrard");
 
-        mImageUrls.add("https://cdn.images.express.co.uk/img/dynamic/67/590x/Virgil-van-Dijk-904489.jpg");
-        mNames.add("Virgil Van Dijk");
+        mImageUrls.add("https://images.shazam.com/coverart/t330000664-b1153532692_s400.jpg");
+        mNames.add("Rapper-Tery");
 
-        mImageUrls.add("https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0001/43/thumb_42725_default_news_size_5.jpeg");
-        mNames.add("Joël Matip");
+        mImageUrls.add("http://e-cdn-images.deezer.com/images/artist/679e733373d9aadfdd8d07a5941b8051/500x500.jpg");
+        mNames.add("illslick");
 
-        mImageUrls.add("https://cdn.images.dailystar.co.uk/dynamic/58/photos/547000/620x/Andy-Robertson-Liverpool-652850.jpg");
-        mNames.add("Andrew Robertson");
+        mImageUrls.add("https://pbs.twimg.com/profile_images/893737747172573185/kLqJU6gZ.jpg");
+        mNames.add("Youngohm");
 
 
-        mImageUrls.add("https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2018/02/04/16/henderson.jpg");
-        mNames.add("Jordan Henderson");
+        mImageUrls.add("https://www.rapisnow.com/blog/wp-content/uploads/2017/10/oak.jpg");
+        mNames.add("OAK");
 
-        mImageUrls.add("https://www.thisisanfield.com/wp-content/uploads/P170913-074-Liverpool_Seville-e1505337388786-600x401.jpg");
-        mNames.add("Alex Oxlade-Chamberlain");
+        mImageUrls.add("http://p1.isanook.com/jo/0/ud/477/2386375/twopee01.jpg");
+        mNames.add("TwoPee");
 
-        mImageUrls.add("https://i2.wp.com/draftfantasyfootball.co.uk/content/wp-content/uploads/2017/08/jamesmilner-cropped_18qsw5185s9lt1jogq9rvx986d.jpg?resize=1280%2C640&ssl=1");
-        mNames.add("James Milner");
-
-        mImageUrls.add("http://givemesport.azureedge.net/images/17/11/21/2df4f758aa8c0dacfa6468a3357be642/960.jpg");
-        mNames.add("Mohamed Salah");
-
-        mImageUrls.add("https://img.maximummedia.ie/joe_co_uk/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lY291ay5tYXhpbXVtbWVkaWEuaWUuczMuYW1hem9uYXdzLmNvbVxcXC93cC1jb250ZW50XFxcL3VwbG9hZHNcXFwvMjAxOFxcXC8wMVxcXC8xNzEzMjIxNVxcXC9HZXR0eUltYWdlcy04OTg3OTA3MTIuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5jby51a1xcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZWNvdWtcXFwvbm8taW1hZ2UucG5nP3Y9NVwifSIsImhhc2giOiI0MTAwNjQ5N2M4NTlkYzAwMWQwOTViYTNmZTJmNGExNWI3YmRjY2U5In0=/gettyimages-898790712.jpg");
-        mNames.add("Roberto Firmino");
-
-        mImageUrls.add("http://cdn.empireofthekop.com/wp-content/uploads/2018/02/mane-picture-640x384.jpg");
-        mNames.add("Sadio Mané");
-
-        mImageUrls.add("http://e2.365dm.com/18/01/16-9/20/skysports-jurgen-klopp-liverpool-swansea_4212383.jpg?20180122233458");
-        mNames.add("Jürgen Klopp");
+        mImageUrls.add("https://i.ytimg.com/vi/iSkDKBx_Ybg/maxresdefault.jpg");
+        mNames.add("KQ");
 
         initRecyclerView();
     }
